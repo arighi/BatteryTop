676d05e3fe23ff23ed5538b6599c1ced27226f8e
Andrea Righi <andrea@betterlinux.com>
2011-09-06 16:02:55 +0200

    provide a human readable description for the known battery informations
    Signed-off-by: Andrea Righi <andrea@betterlinux.com>

--- src/com/arighi/batterytop/BatteryInfo.java
+++ src/com/arighi/batterytop/BatteryInfo.java
@@ -29,9 +29,42 @@ import android.widget.TextView;
 import java.util.Set;
 import java.util.HashMap;
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
 
 /* Handle all battery change events */
 public class BatteryInfo extends BroadcastReceiver {
+    /* Mapping from battery status codes to strings */
+    private static final HashMap<Integer, String> batteryStatus = new HashMap<Integer, String>();
+    static {
+        batteryStatus.put(BatteryManager.BATTERY_STATUS_UNKNOWN,
+                          "unknown");
+        batteryStatus.put(BatteryManager.BATTERY_STATUS_CHARGING,
+                          "charging");
+        batteryStatus.put(BatteryManager.BATTERY_STATUS_DISCHARGING,
+                          "draining");
+        batteryStatus.put(BatteryManager.BATTERY_STATUS_NOT_CHARGING,
+                          "not charging");
+        batteryStatus.put(BatteryManager.BATTERY_STATUS_FULL,
+                          "full");
+    }
+
+    private static final HashMap<Integer, String> batteryHealth = new HashMap<Integer, String>();
+    static {
+        batteryHealth.put(BatteryManager.BATTERY_HEALTH_UNKNOWN, "unknown");
+        batteryHealth.put(BatteryManager.BATTERY_HEALTH_GOOD, "good");
+        batteryHealth.put(BatteryManager.BATTERY_HEALTH_OVERHEAT, "overheat");
+        batteryHealth.put(BatteryManager.BATTERY_HEALTH_DEAD, "dead");
+        batteryHealth.put(BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE, "over_voltage");
+        batteryHealth.put(BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE, "failed");
+    }
+
+    private static final HashMap<Integer, String> batteryPlugged = new HashMap<Integer, String>();
+    static {
+        batteryPlugged.put(0, "unplugged");
+        batteryPlugged.put(BatteryManager.BATTERY_PLUGGED_AC, "AC");
+        batteryPlugged.put(BatteryManager.BATTERY_PLUGGED_USB, "USB");
+    }
+
     private BatteryView bv = null;
 
     private float voltageSpeed = 0;
@@ -51,17 +84,41 @@ public class BatteryInfo extends BroadcastReceiver {
         }
     }
 
+    private String getDescription(int val, HashMap<Integer, String> map) {
+        String name = map.get(val);
+        if (name != null)
+            return name;
+        return "<" + val + ">";
+    }
+
     public void onReceive(Context context, Intent intent) {
         StringBuilder builder = new StringBuilder("Battery (raw data)\n");
 
         /* Show raw informations */
         Set<String> keys = intent.getExtras().keySet();
         for (String key : keys) {
+            int value = intent.getIntExtra(key, 0);
+
             if (key.equals("icon-small")) {
                 continue;
+            } else if (key.equals("status")) {
+                builder.append("  " + key + ": " +
+                               getDescription(value, batteryStatus) + "\n");
+            } else if (key.equals("health")) {
+                builder.append("  " + key + ": " +
+                               getDescription(value, batteryHealth) + "\n");
+            } else if (key.equals("plugged")) {
+                builder.append("  " + key + ": " +
+                               getDescription(value, batteryPlugged) + "\n");
+            } else if (key.equals("voltage")) {
+                builder.append("  " + key + ": " +
+                               value + "mV\n");
+            } else if (key.equals("temperature")) {
+                builder.append("  " + key + ": " +
+                               (value / 10) + "." + (value % 10) + "°C\n");
+            } else {
+                builder.append("  " + key + ": " + value + "\n");
             }
-            int value = intent.getIntExtra(key, 0);
-            builder.append("  " + key + " " + value + "\n");
         }
         builder.append("\nBattery (additional statistics)\n");
 
@@ -79,7 +136,7 @@ public class BatteryInfo extends BroadcastReceiver {
             voltageSpeed = (float)((voltage - startVoltage) * 1000) /
                             (time - startTime);
         }
-        builder.append("  voltage/s " + voltageSpeed + "\n");
+        builder.append("  charge/discharge speed: " + voltageSpeed + "mV/s\n");
 
         /* Evaluate discharge time if unplugged */
         if (plugged == 0) {
@@ -87,7 +144,7 @@ public class BatteryInfo extends BroadcastReceiver {
             String startTimeString = formatter.format(startTime);
 
             builder.append("  start time: " + startTimeString + "\n");
-            builder.append("  start voltage: " + startVoltage + "\n");
+            builder.append("  start voltage: " + startVoltage + "mV\n");
 
             if (voltageSpeed < 0) {
                 float seconds = voltage / -voltageSpeed;
